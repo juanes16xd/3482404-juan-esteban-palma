7fc3dd8f2db78f7efe319b47292385645a96f329
juanpalma1309 <juanestebanpalma15xd@gmail.com>
2026-08-04 14:56:15 -0500

    feat: inicia ejercicio 1 - pares e impares

---
+++ src/Ejercicio1.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //entrada
+        int pares = 0;
+        int impares = 0;
+        int limite = 0;
+        int numeros = Integer.parseInt(JOptionPane.showInputDialog("¿cuantos números va a ingresar?"));
+        int[] guardar = new int[numeros];
+        //proceso
+        for(int i = 0; i < guardar.length; i++) {
+            if(i  % 2 == 0) {
+                pares++;
+            }else{
+                impares++;
+            }
+            if(limite <= guardar.length) {
+                numeros = Integer.parseInt(JOptionPane.showInputDialog("ingrese el número:"));
+                limite++;
+            }
+        }
+        //salida
+        JOptionPane.showMessageDialog(null, "pares registrados:" +pares+ "\n impares registrados:" +impares);
+    }
+}
