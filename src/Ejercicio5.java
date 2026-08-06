9f610509b876d60906ea2eff0cb3715bf6400e16
juanpalma1309 <juanestebanpalma15xd@gmail.com>
2026-08-06 14:29:53 -0500

    feat: inicia ejercicio 5 -busqueda de número en array

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
+import javax.swing.JOptionPane;
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        //entrada
+        int n = Integer.parseInt(JOptionPane.showInputDialog("¿cuantos números va a ingresar:"));
+        int[] numeros = new int[n];
+        String posiciones = "";
+        boolean encontrado = false;
+        //proceso
+        for(int i = 0; i < numeros.length; i++) {
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("ingrese los números:" + (i +1)));
+        }
+        int buscar = Integer.parseInt(JOptionPane.showInputDialog("ingrese el número a buscar:"));
+        for( int p = 0; p < numeros.length; p++) {
+            if(numeros[p] == buscar) {
+                posiciones += "posicion" +p+ "\n";
+                encontrado = true;
+            }
+        }
+        //salida
+        if(encontrado) {
+            JOptionPane.showMessageDialog(null, "número encontrado en la posición: \n" + posiciones);
+        }else{
+            JOptionPane.showMessageDialog(null, "número no encontrado:");
+        }
+    }
 }
+
